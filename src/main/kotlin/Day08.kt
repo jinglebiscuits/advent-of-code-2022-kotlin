4712c7f7d3319f6034a353e79ed3f114ebfa359d
jinglebiscuits <scott.wehby@gmail.com>
2022-12-08 12:42:31 -0600

    day8 part 1

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,23 +1,19 @@
 import java.io.File
-import java.util.*
 
 class Day08 {
 
     val rows = mutableListOf<MutableList<Int>>()
 
     init {
-        File(javaClass.getResource("inputs/day8input_test.txt").toURI()).readLines().map {
+        File(javaClass.getResource("inputs/day8input.txt").toURI()).readLines().map {
             rows.add(mutableListOf())
             it.map { char -> rows.last().add(char.digitToInt()) }
-            println(it)
         }
-        println(rows.flatten())
-        println(rows.flatten().filterIndexed { index, i -> (index - 1) % (rows[0].size) == 0 })
         part1()
     }
 
     private fun part1() {
-        val hiddenTrees = Collections.nCopies(rows.size, (Collections.nCopies(rows[0].size, 1)).toMutableList())
+        val hiddenTrees = Array(rows.size) { IntArray(rows[0].size) { 1 } }
         for (row in 0 until rows.size) {
             for (column in 0 until rows[0].size) {
                 if (isSeenFromEdge(row, column)) {
@@ -25,12 +21,13 @@ class Day08 {
                 }
             }
         }
-        println(hiddenTrees.sumOf {
+        val sum = hiddenTrees.sumOf {
             it.sum()
-        })
+        }
+        println(rows.size * rows[0].size - sum)
     }
 
-    private fun isSeenFromEdge(rowPos: Int, columnPos: Int) : Boolean {
+    private fun isSeenFromEdge(rowPos: Int, columnPos: Int): Boolean {
         if (rowPos == 0 || rowPos == rows.size - 1 || columnPos == 0 || columnPos == rows[0].size - 1) {
             return true
         } else {
@@ -42,7 +39,6 @@ class Day08 {
             val seenFromBottom = !column.subList(rowPos + 1, rows.size).any { it >= treeHeight }
             val seenFromLeft = !rows[rowPos].subList(0, columnPos).any { it >= treeHeight }
             val seenFromRight = !rows[rowPos].subList(columnPos + 1, rows[rowPos].size).any { it >= treeHeight }
-            println("row $rowPos column $columnPos")
             return seenFromTop || seenFromBottom || seenFromLeft || seenFromRight
         }
     }
