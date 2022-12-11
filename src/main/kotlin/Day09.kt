8c9525a30c45c5361a6f167f5a72c975b4fca023
jinglebiscuits <scott.wehby@gmail.com>
2022-12-11 15:17:42 -0600

    checkpoint

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,20 +1,23 @@
-import java.awt.Point
 import java.io.File
+import java.util.*
 import kotlin.math.atan
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 class Day09 {
 
-    private val headLocation = mutableSetOf(Point(0, 0))
-    private var currentHeadLocation = Point(0, 0)
     private val tailLocation = mutableSetOf(Point(0, 0))
-    private var currentTailLocation = Point(0, 0)
+    private val knots = List(10) { Point(0, 0) }
+    val file = File("part2.txt")
 
     init {
+        file.writeText("")
+        file.createNewFile()
+        file.path
         File(javaClass.getResource("inputs/day9input.txt").toURI()).readLines().map {
             println(it)
             processMove(it.split(" "))
+            file.appendText(it + "\n" + tailLocation.size.toString() + "\n" + knots.last() + "\n\n")
         }
         println("answer: ${tailLocation.size}")
     }
@@ -45,31 +48,40 @@ class Day09 {
     }
 
     private fun moveUp() {
-        currentHeadLocation = Point(currentHeadLocation.x, currentHeadLocation.y + 1)
-        headLocation.add(currentHeadLocation)
-        moveTail()
+        knots[0].y += 1
+        moveKnots()
     }
 
     private fun moveDown() {
-        currentHeadLocation = Point(currentHeadLocation.x, currentHeadLocation.y - 1)
-        headLocation.add(currentHeadLocation)
-        moveTail()
+        knots[0].y -= 1
+        moveKnots()
     }
 
     private fun moveLeft() {
-        currentHeadLocation = Point(currentHeadLocation.x - 1, currentHeadLocation.y)
-        headLocation.add(currentHeadLocation)
-        moveTail()
+        knots[0].x -= 1
+        moveKnots()
     }
 
     private fun moveRight() {
-        currentHeadLocation = Point(currentHeadLocation.x + 1, currentHeadLocation.y)
-        headLocation.add(currentHeadLocation)
-        moveTail()
+        knots[0].x += 1
+        moveKnots()
     }
 
-    private fun moveTail() {
-        val diff = Point(currentHeadLocation.x - currentTailLocation.x, currentHeadLocation.y - currentTailLocation.y)
+    private fun moveKnots() {
+        for (i in 1 until knots.size) {
+            val result = moveKnot(knots[i], knots[i-1])
+            knots[i].x = result.x
+            knots[i].y = result.y
+        }
+        if (!tailLocation.contains(knots.last())) {
+            tailLocation.add(knots.last())
+            file.appendText("adding ${knots.last()} to list\n")
+        }
+    }
+
+    private fun moveKnot(follower: Point, leader: Point): Point {
+        val diff = Point(leader.x - follower.x, leader.y - follower.y)
+        var result = Point(follower.x, follower.y)
         if (sqrt(diff.x.toDouble().pow(2.toDouble()) + diff.y.toDouble().pow(2.toDouble())) > 1.5) {
             var direction = 90.0
             try {
@@ -83,24 +95,41 @@ class Day09 {
                 direction = -90.0
             }
             if (direction == 0.0) {
-                currentTailLocation = Point(currentTailLocation.x + 1, currentTailLocation.y)
+                result = Point(follower.x + 1, follower.y)
             } else if (direction > 0.0 && direction < 90.0) {
-                currentTailLocation = Point(currentTailLocation.x + 1, currentTailLocation.y + 1)
+                result = Point(follower.x + 1, follower.y + 1)
             } else if (direction == 90.0) {
-                currentTailLocation = Point(currentTailLocation.x, currentTailLocation.y + 1)
+                result = Point(follower.x, follower.y + 1)
             } else if (direction > 90.0 && direction < 180.0) {
-                currentTailLocation = Point(currentTailLocation.x - 1, currentTailLocation.y + 1)
+                result = Point(follower.x - 1, follower.y + 1)
             } else if (direction == 180.0) {
-                currentTailLocation = Point(currentTailLocation.x - 1, currentTailLocation.y)
+                result = Point(follower.x - 1, follower.y)
             } else if (direction > 180.0 && direction < 270.0) {
-                currentTailLocation = Point(currentTailLocation.x - 1, currentTailLocation.y - 1)
+                result = Point(follower.x - 1, follower.y - 1)
             } else if (direction == -90.0) {
-                currentTailLocation = Point(currentTailLocation.x, currentTailLocation.y - 1)
+                result = Point(follower.x, follower.y - 1)
             } else {
-                currentTailLocation = Point(currentTailLocation.x + 1, currentTailLocation.y - 1)
+                result = Point(follower.x + 1, follower.y - 1)
             }
-            tailLocation.add(currentTailLocation)
         }
+        return result
+    }
+}
 
+class Point(var x: Int, var y: Int) {
+    override fun equals(other: Any?): Boolean {
+        return if (other is Point) {
+            x == other.x && y == other.y
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(x, y)
+    }
+
+    override fun toString(): String {
+        return "java.awt.Point[x=$x,y=$y]"
     }
 }
